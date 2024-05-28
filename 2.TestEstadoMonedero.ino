8131402d6946048af25b4252321a5b5a36f5e1d2
FabianUNIT <fabian.ayala@uelectronics.com>
2024-05-28 09:38:37 -0600

    Codigo de prueba para Ramas

---
+++ 2.TestEstadoMonedero.ino
@@ -0,0 +1,85 @@
+const int Selector_Monedas_Pin = 2;//pin al cual estara conectada la señal del monedero
+
+int Lectura_Pin_Moneda;
+
+int Contador_Monedas = 0;
+
+const int Led_Boton = A1;//pin al cual estara conectado un led, para indicar cuando una moneda es aceptada.
+
+//Pines para el boton con led
+    #define Boton A0 // // Arduino Mega 12
+    #define Boton2 A2 // // Arduino Mega 12
+
+//Variable para el estado del boton de inicio
+int estadoBoton, estadoBoton_ant;
+
+//Variable para el estado del boton de inicio
+int estadoBoton2, estadoBoton_ant2;
+
+//Etapas de funcionamiento
+int etapa = 0;
+
+void setup() {
+  // Inicializamos el puerto serial.
+  Serial.begin(9600);
+  
+  //Interrupcion para el Selector de Monedas
+  pinMode(Selector_Monedas_Pin, INPUT);
+  attachInterrupt(digitalPinToInterrupt(Selector_Monedas_Pin), Selector_Monedas_Interrupt, RISING);
+
+  //Asignamos el pin para el led como salida
+  pinMode(Led_Boton, OUTPUT);
+   pinMode(Boton2, INPUT);
+   pinMode(Boton, INPUT);
+  
+}
+
+void loop() {
+    Leer_Boton();
+//De acuerdo con la Tabla de Estados se tiene la siguiente lógica
+  if (etapa == 0) {
+    if (Contador_Monedas >= 1)
+    {
+      enciendeBoton();
+      if (estadoBoton == 1) {
+        apagaBoton();
+        etapa = 1;
+        estadoBoton = 0;
+      }
+    }
+  }
+
+ if (etapa == 1) {
+ if (estadoBoton2 == 1) {
+        etapa = 0;
+        estadoBoton2 = 0;
+        Contador_Monedas--;
+      }
+  }
+}
+
+void Selector_Monedas_Interrupt() {
+
+ Lectura_Pin_Moneda = digitalRead(Selector_Monedas_Pin);
+
+  if (Lectura_Pin_Moneda == 1){
+    Contador_Monedas++;
+  }
+}
+
+void Leer_Boton () {
+  estadoBoton = digitalRead(Boton);
+  estadoBoton_ant = estadoBoton;
+  estadoBoton2 = digitalRead(Boton2);
+  estadoBoton_ant2 = estadoBoton2;
+}
+
+void enciendeBoton()
+{
+  digitalWrite(Led_Boton, HIGH);
+}
+
+void apagaBoton()
+{
+  digitalWrite(Led_Boton, LOW);
+}
